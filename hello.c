aa53400c2a6449c2628d9467de07584e5aeb3790
Yumicn <zymjdc@qq.com>
2016-07-15 14:20:47 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+
+int getAge();
+char* getNAme();
 //main
 int main() {
-	printf("\nHello World\n\n");
+	char* name = getName();
+	int age = getAge();
+	printf("\nHello World! My name is: %s, age: %d\n\n", name, age);
+}
+
+int getAge() {
+	int age = 15;
+	return age;
+}
+
+char* getName() {
+	char* name = "rose";
+	return name;
 }
